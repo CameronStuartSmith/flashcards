5a489b7540e3409c8ccfc802f635380e7b57a005
Cameron Smith <cameronstuartsmith@gmail.com>
2018-05-09 23:37:41 -0500

    Added FlatView

--- components/DeckListView.js
+++ components/DeckListView.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import DeckListItem from './DeckListItem';
 
 class DeckListView extends Component {
+
+	renderItem = ({ item }) => {
+		return <DeckListItem data={item} />
+	}
+
 	render() {
 		const { decks } = this.props;
 
+		var arr = Object.keys(decks).map(key => {
+			return {
+				key,
+				...decks[key]
+			}
+		});
+
 		return (
-			<View>
-				{
-					Object.keys(decks).map(key => {
-						return <DeckListItem data={decks[key]} />
-					})
-				}
+			<View style={{flex: 1}}>
+				<FlatList data={arr} renderItem={this.renderItem} style={styles.list} />
 			</View>
 		);
 	}
 }
+
+
+const styles = StyleSheet.create({
+	list: {
+		
+	}
+});
 
 export default DeckListView;
